84d65473e3276e76b01c4973396e24298f717c60
Pedrazzoli Daniele <danielepedrazzoli2000@gmail.com>
2025-11-30 21:23:27 +0100

    Added the code in kotlin for traslate selected text

--- android/app/src/main/kotlin/com/example/langyu/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/langyu/MainActivity.kt
@@ -1,5 +1,43 @@
 package com.example.langyu
 
+import android.content.Intent
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    private val CHANNEL = "app.channel.shared.data"
+    private var sharedText: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        handleIntent(intent)
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+                call,
+                result ->
+            if (call.method == "getSharedText") {
+                result.success(sharedText)
+                sharedText = null
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent?) {
+        if (intent?.action == Intent.ACTION_PROCESS_TEXT) {
+            sharedText = intent.getStringExtra(Intent.EXTRA_PROCESS_TEXT)
+        }
+    }
+}
